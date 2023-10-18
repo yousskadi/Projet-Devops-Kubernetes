df3c88f85dd83da1ed90131e3a35d3e6f39f0b22
Nawfal Eddaoudi <nawfal.eddaoudi@gmail.com>
2023-10-18 11:27:19 +0000

    up

--- config/db.py
+++ config/db.py
@@ -1,14 +1,7 @@
-# from sqlalchemy import create_engine, MetaData
-
-# engine = create_engine("postgresql://admin:password@db:5432/storedb")
-
-# meta = MetaData()
-
-# conn = engine.connect()
-
-
 from sqlalchemy import create_engine, MetaData
 import base64
+
+######## Prevent exposing password in plaintext in fastapi application and in config files.
 
 # # Récupérer le mot de passe du Secret
 password_base64 = "UHJvamVjdERldm9wczIwMjMh"  # Remplacez par la valeur réelle depuis votre Secret
