b520c6305dbe10b49ff7dc5f45c06f9b381cfc5f
VikingHacker1 <114841948+VikingHacker1@users.noreply.github.com>
2023-11-02 18:01:51 +0100

    Update db.py

--- config/db.py
+++ config/db.py
@@ -4,9 +4,9 @@ import base64
 # Récupérer le mot de passe du Secret
 password_base64 = "UHJvamVjdERldm9wczIwMjMh"  # Remplacez par la valeur réelle depuis votre Secret
 password = base64.b64decode(password_base64).decode("utf-8")
-print(password)
+print("This is my password that I am looking for", password)
 engine = create_engine(f"postgresql://admin:{password}@db:5432/storedb")
-print(engine)
+print("This is the engine value that I am looking for", engine)
 meta = MetaData()
 
 conn = engine.connect()
