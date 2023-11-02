d249aa2e3fe914be110f194aa1a101dcab753773
VikingHacker1 <114841948+VikingHacker1@users.noreply.github.com>
2023-11-02 18:16:21 +0100

    Update db.py

--- config/db.py
+++ config/db.py
@@ -2,11 +2,11 @@ from sqlalchemy import create_engine, MetaData
 import base64
 
 # Récupérer le mot de passe du Secret
-password_base64 = "UHJvamVjdERldm9wczIwMjMh"  # Remplacez par la valeur réelle depuis votre Secret
-password = base64.b64decode(password_base64).decode("utf-8")
-print("This is my password that I am looking for", password)
-engine = create_engine(f"postgresql://admin:{password}@db:5432/storedb")
-print("This is the engine value that I am looking for", engine)
+# password_base64 = "UHJvamVjdERldm9wczIwMjMh"  # Remplacez par la valeur réelle depuis votre Secret
+# password = base64.b64decode(password_base64).decode("utf-8")
+
+
+engine = create_engine(f"postgresql://admin:password@db:5432/storedb")
 meta = MetaData()
 
 conn = engine.connect()
