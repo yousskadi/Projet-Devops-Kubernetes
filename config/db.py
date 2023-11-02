ac0a480a248f1f2d8ec13c12ebbdc166d1ba6abe
VikingHacker1 <114841948+VikingHacker1@users.noreply.github.com>
2023-11-02 19:03:36 +0100

    Update db.py

--- config/db.py
+++ config/db.py
@@ -6,7 +6,7 @@ import base64
 # password = base64.b64decode(password_base64).decode("utf-8")
 
 
-engine = create_engine(f"postgresql://admin:password@db:5432/storedb")
+engine = create_engine("postgresql://admin:password@db:5432/storedb")
 meta = MetaData()
 
 conn = engine.connect()
